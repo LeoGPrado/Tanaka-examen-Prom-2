a1d8b0c2c6729e82d7ad59d497163f59ed37fda6
Mayhem <alexandersaca3@gmail.com>
2026-05-28 18:51:24 -0500

    Ajustes y añadidos generales
    - Se añade TMP y sus dependencias
    - Se cambia a Old Input System (no se guardó bien al jalarlo directo del master branch)
    - Se crea ambos scripts tanto del puzzle como del menú
    - Se quito lo del tutorialInfo del urp
    hecho por alexander 👻

---
+++ Assets/AlexAssets/Scripts/Puzzle_Sequence.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Puzzle_Sequence : MonoBehaviour
+{
+    private const int MAX_BUTTON = 9;
+    [SerializeField] Button[] slots = new Button[MAX_BUTTON];
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
