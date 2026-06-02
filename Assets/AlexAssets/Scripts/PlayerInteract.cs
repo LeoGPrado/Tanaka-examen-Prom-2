baf41405649fa243043e197436c9a2e40645a89c
Mayhem <alexandersaca3@gmail.com>
2026-06-02 16:02:26 -0500

    Interaccion completo

---
+++ Assets/AlexAssets/Scripts/PlayerInteract.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    [SerializeField] float interactRange = 1f;
+    [SerializeField] LayerMask interactLayer, obstructLayer;
+    [SerializeField] Transform interactPoint;
+    IInteractable currentInteractable;
+    void Update()
+    { 
+        InteractDetector();
+        InteractInput();
+    }
+    void InteractDetector()
+    {
+        float effectiveRange = interactRange;
+        if (Physics.Raycast(interactPoint.position, interactPoint.forward, out RaycastHit obstructionHit, interactRange, obstructLayer))
+        {
+            effectiveRange = obstructionHit.distance + 0.01f;
+        }
+        if (Physics.Raycast(interactPoint.position, interactPoint.forward, out RaycastHit hit, Mathf.Min(effectiveRange, interactRange), interactLayer))
+        {
+           IInteractable interactable = hit.collider.GetComponentInParent<IInteractable>();
+           if (interactable != null)
+            {
+                SetCurrentInteractable(interactable);
+                return;
+            }
+        }
+        ClearInteractable();
+    }
+    void InteractInput()
+    {
+        if (currentInteractable != null && Input.GetKeyDown(KeyCode.E))
+        {
+            currentInteractable.Interact(interactPoint.position);
+        }
+    }
+    void SetCurrentInteractable(IInteractable interactable)
+    {
+        currentInteractable = interactable;
+    }
+    void ClearInteractable()
+    {
+        currentInteractable = null;
+    }
+}
