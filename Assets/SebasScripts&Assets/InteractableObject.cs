a4c25519b4287f4a5b3ff0aad0cf1ddc98412870
YabelyGutierrez <yanlie.arts@gmail.com>
2026-06-04 13:25:45 -0500

    Script de interacción provisional
    -Creación de interacción por Raycast y UnityEvents

---
+++ Assets/SebasScripts&Assets/InteractableObject.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractableObject : MonoBehaviour
+{
+
+    [SerializeField] private UnityEvent onInteract;
+    [SerializeField] private string interactText = "Presiona E";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnInteract()
+    {
+        onInteract?.Invoke();
+    }
+}
