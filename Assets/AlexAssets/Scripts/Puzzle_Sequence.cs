d1e478295a07c358661462e68c3c2577885833f7
Mayhem <alexandersaca3@gmail.com>
2026-06-04 16:38:33 -0500

    ajustes
    si

--- Assets/AlexAssets/Scripts/Puzzle_Sequence.cs
+++ Assets/AlexAssets/Scripts/Puzzle_Sequence.cs
@@ -6,7 +6,6 @@ public class Puzzle_Sequence : MonoBehaviour
 {
     [SerializeField] Button[] numpadButtons;
     [SerializeField] Image[] sequenceLights;
-
     [Header("Settings")]
     [SerializeField] Color lightOffColor = Color.black;
     [SerializeField] Color lightCorrectColor = Color.green;
@@ -14,90 +13,74 @@ public class Puzzle_Sequence : MonoBehaviour
     [SerializeField] UnityEngine.Events.UnityEvent OnSolved;
     private int currentSequenceStep = 0; 
     private int expectedNextValue = 0;   
-    private bool isLockedOut = false;    
+    private bool disablePuzzle = false;    
     void Start()
     {
         InitializePuzzle();
     }
 
-    private void InitializePuzzle()
+    void InitializePuzzle()
     {
         for (int i = 0; i < numpadButtons.Length; i++)
         {
             int buttonValue = i + 1; 
-            Button btn = numpadButtons[i];
+            Button button = numpadButtons[i];
 
-            if (btn != null)
-            {
-                btn.onClick.AddListener(() => OnButtonPressed(buttonValue));
-            }
+            if (button != null) button.onClick.AddListener(() => OnButtonPressed(buttonValue));
         }
 
         ResetLights();
     }
 
-    private void OnButtonPressed(int pressedValue)
+    void OnButtonPressed(int pressedValue)
     {
-        if (isLockedOut || currentSequenceStep >= 9) return;
+        if (disablePuzzle || currentSequenceStep >= 9) return;
 
-        if (currentSequenceStep == 0 || pressedValue == expectedNextValue)
-        {
-            ProcessCorrectPress(pressedValue);
-        }
-        else
-        {
-            ProcessError();
-        }
+        if (currentSequenceStep == 0 || pressedValue == expectedNextValue) ProcessCorrectPress(pressedValue);
+        else ProcessError();
     }
 
-    private void ProcessCorrectPress(int pressedValue)
+    void ProcessCorrectPress(int pressedValue)
     {
-        if (currentSequenceStep < sequenceLights.Length)
-        {
+        if (currentSequenceStep < sequenceLights.Length) 
             sequenceLights[currentSequenceStep].color = lightCorrectColor;
-        }
 
         currentSequenceStep++;
 
         expectedNextValue = (pressedValue % 9) + 1;
-        if (currentSequenceStep >= 9)
-        {
-            CompletePuzzle();
-        }
+        if (currentSequenceStep >= 9) CompletePuzzle();
     }
 
-    private void ProcessError()
+    void ProcessError()
     {
         StartCoroutine(ErrorResetRoutine());
     }
 
-    private IEnumerator ErrorResetRoutine()
+    IEnumerator ErrorResetRoutine()
     {
-        isLockedOut = true; 
+        WaitForSeconds waitForSeconds = new(0.5f);
+        disablePuzzle = true; 
 
         if (currentSequenceStep < sequenceLights.Length)
         {
             sequenceLights[currentSequenceStep].color = lightErrorColor;
         }
-        WaitForSeconds waitForSeconds = new(0.5f);
         yield return waitForSeconds;
 
         currentSequenceStep = 0;
         expectedNextValue = 0;
         ResetLights();
 
-        isLockedOut = false;
+        disablePuzzle = false;
     }
 
-    private void ResetLights()
+    void ResetLights()
     {
-        foreach (Image light in sequenceLights)
-        {
+        foreach (Image light in sequenceLights) 
             if (light != null) light.color = lightOffColor;
-        }
     }
 
-    private void CompletePuzzle()
+    void CompletePuzzle()
     {
         Debug.Log("[Puzzle Caja Fuerte] Secuencia completada. Abriendo compartimento...");
         OnSolved?.Invoke();
