5745b3ffbd8d049c5134a7808a865092ff0f3918
Mayhem <alexandersaca3@gmail.com>
2026-06-04 16:42:32 -0500

    Apartado de creditos

--- Assets/AlexAssets/Scripts/MainMenu.cs
+++ Assets/AlexAssets/Scripts/MainMenu.cs
@@ -4,14 +4,18 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] string sceneName;
+    [SerializeField] string gameNameScene;
+    [SerializeField] string creditsNameScene;
     [SerializeField] Button playButton;
     [SerializeField] Button exitButton;
+    [SerializeField] Button creditsButton;
     void Awake()
     {
         if (playButton != null) playButton.onClick.AddListener(()=> LoadGameScene());
         if (exitButton != null) exitButton.onClick.AddListener(() => ExitGame());
+        if (creditsButton != null) creditsButton.onClick.AddListener(() => LoadCreditsScene());
     }
-    void LoadGameScene() => SceneManager.LoadScene(sceneName);
+    void LoadGameScene() => SceneManager.LoadScene(gameNameScene);
+    void LoadCreditsScene() => SceneManager.LoadScene(creditsNameScene);
     void ExitGame() => Application.Quit();
 }
