e2df26f28aac32041a7f8f8e1e7da975c8d75997
Yabely Gutierrez <yanlie.arts@gmail.com>
2026-06-05 13:51:53 -0500

    Arreglo de cajones y rotación puertas
    - Arreglo de bug con la rotación de las puertas de los muebles
    - Funcionamiento correcto de los cajones

--- Assets/YanScripts/DoorCodePuzzle.cs
+++ Assets/YanScripts/DoorCodePuzzle.cs
@@ -3,37 +3,29 @@ using UnityEngine;
 public class DoorCodePuzzle : MonoBehaviour
 {
 
-    private RotateDoors rt;
+    [SerializeField] private RotateDoors rt;
     [SerializeField] private Transform pivot;
     [SerializeField] private GameObject codePanel;
 
-    [SerializeField] private float openSpeed = 2f;
-    [SerializeField] private float openingAngle = 90f; 
 
     [SerializeField] private bool locked;
     [SerializeField] private bool opening;
-    private Quaternion closingRotation;
-    private Quaternion openingRotation;
-    
+
 
 
 
 
     void Start()
     {
+        opening = false;
         locked = true;
         codePanel.SetActive(false);
-        closingRotation = pivot.rotation;
-        openingRotation = Quaternion.Euler(pivot.eulerAngles + new Vector3(0, openingAngle, 0));
+      
     }
 
     void Update()
     {
-        if (opening)
-        {
-            pivot.rotation = Quaternion.Slerp(pivot.rotation, openingRotation, Time.deltaTime * openSpeed);
-            rt.Action();
-        }
+        
     }
 
     public void OnInteract()
@@ -45,13 +37,14 @@ public class DoorCodePuzzle : MonoBehaviour
     {
         if (!locked) return;
         codePanel.SetActive(true);
+        
     }
 
     public void UnlockDoorWithCode()
     {
         locked = false;
         opening = true;
-
+        if(opening) rt.Action();
         codePanel.SetActive(false);
     }
 }
